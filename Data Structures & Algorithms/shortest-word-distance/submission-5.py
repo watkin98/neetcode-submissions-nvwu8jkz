5a3411f7854837290b14623a6e157bdc7750131d
William Atkin <122399822+watkin98@users.noreply.github.com>
2026-06-24 15:59:23 -0700

    Add: shortest-word-distance - submission-5

---
+++ Data Structures & Algorithms/shortest-word-distance/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        w1, w2 = -1, -1
+        minLen = len(wordsDict)
+
+        for i in range(len(wordsDict)):
+            if wordsDict[i] == word1:
+                w1 = i
+            elif wordsDict[i] == word2:
+                w2 = i
+
+            if w1 != -1 and w2 != -1:
+                minLen = min(minLen, abs(w2 - w1))
+            
+        return minLen
