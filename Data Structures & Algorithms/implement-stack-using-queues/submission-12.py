ccb8605c9cf6e978d88d236527cfb197d6b0b47b
William Atkin <122399822+watkin98@users.noreply.github.com>
2026-09-04 17:29:12 -0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-12.py
@@ -0,0 +1,32 @@
+class MyStack:
+
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        self.q1.append(x)
+        for _ in range(len(self.q1) - 1):
+            self.q1.append(self.q1.popleft())
+
+    def pop(self) -> int:
+        res = self.q1.popleft()
+
+        for _ in range(len(self.q1) - 1):
+            self.q1.append(self.q1.popleft())
+        
+        return res
+
+    def top(self) -> int:
+        return self.q1[0]
+
+    def empty(self) -> bool:
+        return len(self.q1) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+
+        t_count = defaultdict(int)
+        for c in t:
+            t_count[c] += 1
+        
+        s_count = defaultdict(int)
+        seen, need = 0, len(t)
+        l = 0
+        res = [-1, -1, len(s) + 1]
+        for r in range(len(s)):
+            c = s[r]
+
+            if c in t_count and s_count[c] < t_count[c]:
+                s_count[c] += 1
+                seen += 1
+            elif c in t_count:
+                c_count[c] += 1
+            
+            while seen == need:
+                if (r-l+1) < res[2]:
+                    res = [l, r, r-l+1]
+
+                if s[l] in s_count and s_count[s[l]] - 1 < t_count[s[l]]:
+                    s_count[s[l]] -= 1
+                    seen -= 1
+                elif s[l] in s_count:
+                    s_count[s[l]] -= 1
+                l += 1
+
+        left, right, length = res
+        return s[left:right+1] if length != len(s) + 1 else ""
