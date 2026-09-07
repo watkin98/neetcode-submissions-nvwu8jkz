cbaf049b0acb82232be7755c2cd3b5dc85dfcce2
William Atkin <122399822+watkin98@users.noreply.github.com>
2026-09-07 15:54:11 -0700

    Add: search-in-rotated-sorted-array-ii - submission-1

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-1.py
@@ -0,0 +1,47 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        pivot = self.findPivot(nums, target)
+        print(f"Pivot {pivot}: {nums[pivot]}")
+        return self.binarySearch(nums, 0, pivot-1, target) or self.binarySearch(nums, pivot, len(nums) - 1, target)
+
+    def findPivot(self, nums, target) -> int:
+        l, r = 0, len(nums) - 1
+        pivot = nums[0]
+
+        while l <= r:
+            if nums[l] <= nums[r] and nums[l] < nums[pivot]:
+                pivot = l
+                break
+            elif nums[l] <= nums[r]:
+                break
+
+            m = l + ((r-l) // 2)
+            if nums[m] < nums[pivot]:
+                pivot = m
+
+            print(f"l, r: {l}, {r}\nnums[{m}]: {nums[m]}")
+            if nums[l] <= nums[m]:
+                if nums[l] < nums[pivot]:
+                    pivot = l
+                l = m + 1
+                while nums[l+1] == nums[l]:
+                    l += 1
+            else:
+                r = m - 1
+                while nums[r-1] == nums[r]:
+                    r -= 1
+        
+        return pivot
+        
+    def binarySearch(self, nums, l, r, target) -> bool:
+        while l <= r:
+            m = l + ((r-l) // 2)
+
+            if nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                return True
+
+        return False
