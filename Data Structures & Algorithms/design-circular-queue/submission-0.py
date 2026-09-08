a8646b7ff0a757f733089be98c627e79f0f06a94
William Atkin <122399822+watkin98@users.noreply.github.com>
2026-09-07 22:13:17 -0700

    Add: design-circular-queue - submission-0

---
+++ Data Structures & Algorithms/design-circular-queue/submission-0.py
@@ -0,0 +1,58 @@
+class MyCircularQueue:
+    class Node:
+        def __init__(self, val=None):
+            self.value = val
+            self.next = None
+
+    def __init__(self, k: int):
+        self.capacity = k
+        self.length = 0
+        self.head = self.Node()
+        self.tail = None
+
+    def enQueue(self, value: int) -> bool:
+        if self.length == self.capacity:
+            return False
+        
+        curr = self.head
+        while curr.next != None:
+            curr = curr.next
+        curr.next = self.tail = self.Node(value)
+        
+        self.length += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.length == 0:
+            return False
+
+        res = self.head.value
+        self.head = self.head.next
+        self.length -= 1
+        return True
+
+    def Front(self) -> int:
+        if self.length == 0:
+            return -1
+        return self.head.value
+
+    def Rear(self) -> int:
+        if self.length == 0:
+            return -1
+        return self.tail.value
+
+    def isEmpty(self) -> bool:
+        return self.capacity == 0
+
+    def isFull(self) -> bool:
+        return self.length == self.capacity
+
+
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
